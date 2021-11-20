d2f5d668a15e9f38cff765b7b05fc988d773c209
Nelson Nobre <technobre@gmail.com>
2021-11-20 23:05:30 +0000

    Start project

---
+++ src/Attributes/BasicAuthentication.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace PowerUtils.AspNetCore.Authentication.BasicAuth.Attributes
+{
+    public class BasicAuthentication : AuthorizeAttribute
+    {
+        public const string AUTHENTICATION_SCHEME = "Basic";
+
+        public BasicAuthentication()
+        {
+            this.AuthenticationSchemes = AUTHENTICATION_SCHEME;
+        }
+    }
+}
